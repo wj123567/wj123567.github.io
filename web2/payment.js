6766ffbb9b198c56960fe9ba22562d4f8ea36831
WZLiew <wengzhen123@gmail.com>
2023-10-01 14:23:49 +0800

    update

--- web2/payment.js
+++
@@ -1,6 +0,0 @@
-<script>
-                var pressedButton = document.getElementsByClassName("checkout_btn")[0];
-                pressedButton.addEventListener("click", function (event){
-               alert("You have pressed the 'Proceed' button !") 
-                })
-        </script>
